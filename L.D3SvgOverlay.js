6da9e89da4981d618b0892a43e2785f18d92c241
Kirill Zhuravlev <kirillyz@gmail.com>
2015-09-19 21:02:11 +0200

    #1 Added scale to projection object;  Updated README.md;  Some clean-up and new minified vesion.

--- L.D3SvgOverlay.js
+++ L.D3SvgOverlay.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014 Teralytics AG
+ * Copyright 2015 Teralytics AG
  *
  * @author Kirill Zhuravlev <kirill.zhuravlev@teralytics.ch>
  *
@@ -14,29 +14,41 @@ if (typeof L == "undefined") {
 }
 
 // Tiny stylesheet bundled here instead of a separate file
-d3.select("head")
-    .append("style")
-    .attr("type", "text/css")
-    .text("svg.d3-overlay{pointer-events:none;position:absolute;}svg.d3-overlay>g.origin *{pointer-events:visiblePainted;}");
+if (L.version >= "1.0") {
+    d3.select("head")
+        .append("style").attr("type", "text/css")
+        .text("g.d3-overlay *{pointer-events:visiblePainted;}");
+}
 
 // Class definition
 L.D3SvgOverlay = (L.version < "1.0" ? L.Class : L.Layer).extend({
     includes: (L.version < "1.0" ? L.Mixin.Events : []),
 
-    _isDef: function(a){ return typeof a != "undefined" },
+    _undef: function(a){ return typeof a == "undefined" },
 
     _options: function (options) {
-        if (!this._isDef(options)) {
+        if (this._undef(options)) {
             return this.options;
         }
-        options.zoomHide = !this._isDef(options.zoomHide) ? false : options.zoomHide;
-        options.zoomDraw = !this._isDef(options.zoomDraw) ? true : options.zoomDraw;
+        options.zoomHide = this._undef(options.zoomHide) ? false : options.zoomHide;
+        options.zoomDraw = this._undef(options.zoomDraw) ? true : options.zoomDraw;
 
         return this.options = options;
     },
 
+    _disableLeafletRounding: function(){
+        this._leaflet_round = L.Point.prototype._round;
+        L.Point.prototype._round = function(){ return this; };
+    },
+
+    _enableLeafletRounding: function(){
+        L.Point.prototype._round = this._leaflet_round;
+    },
+
     draw: function () {
+        this._disableLeafletRounding();
         this._drawCallback(this.selection, this.projection, this.map.getZoom());
+        this._enableLeafletRounding();
     },
 
     initialize: function (drawCallback, options) { // (Function(selection, projection)), (Object)options
@@ -46,9 +58,11 @@ L.D3SvgOverlay = (L.version < "1.0" ? L.Class : L.Layer).extend({
 
     // Handler for "viewreset"-like events, updates scale and shift after the animation
     _zoomChange: function (evt) {
-        var newZoom = this._isDef(evt.zoom) ? evt.zoom : this.map._zoom; // "viewreset" event in Leaflet has not zoom/center parameters like zoomanim
+        this._disableLeafletRounding();
+        var newZoom = this._undef(evt.zoom) ? this.map._zoom : evt.zoom; // "viewreset" event in Leaflet has not zoom/center parameters like zoomanim
         this._zoomDiff = newZoom - this._zoom;
         this._scale = Math.pow(2, this._zoomDiff);
+        this.projection.scale = this._scale;
         this._shift = this.map.latLngToLayerPoint(this._wgsOrigin)
             ._subtract(this._wgsInitialShift.multiplyBy(this._scale));
 
@@ -57,6 +71,7 @@ L.D3SvgOverlay = (L.version < "1.0" ? L.Class : L.Layer).extend({
         this._rootGroup.attr("transform", shift.concat(scale).join(""));
 
         if (this.options.zoomDraw) { this.draw() }
+        this._enableLeafletRounding();
     },
 
     onAdd: function (map) {
@@ -72,7 +87,7 @@ L.D3SvgOverlay = (L.version < "1.0" ? L.Class : L.Layer).extend({
         } else {
             this._svg = L.svg();
             map.addLayer(this._svg);
-            this._rootGroup = d3.select(this._svg._rootGroup);
+            this._rootGroup = d3.select(this._svg._rootGroup).classed("d3-overlay", true);
         }
         this._rootGroup.classed("leaflet-zoom-hide", this.options.zoomHide);
         this.selection = this._rootGroup;
@@ -88,18 +103,19 @@ L.D3SvgOverlay = (L.version < "1.0" ? L.Class : L.Layer).extend({
         // Create projection object
         this.projection = {
             latLngToLayerPoint: function (latLng, zoom) {
-                zoom = _layer._isDef(zoom) ? zoom : _layer._zoom;
+                zoom = _layer._undef(zoom) ? _layer._zoom : zoom;
                 var projectedPoint = _layer.map.project(L.latLng(latLng), zoom)._round();
                 return projectedPoint._subtract(_layer._pixelOrigin);
             },
             layerPointToLatLng: function (point, zoom) {
-                zoom = _layer._isDef(zoom) ? zoom : _layer._zoom;
+                zoom = _layer._undef(zoom) ? _layer._zoom : zoom;
                 var projectedPoint = L.point(point).add(_layer._pixelOrigin);
                 return _layer.map.unproject(projectedPoint, zoom);
             },
             unitsPerMeter: 256 * Math.pow(2, _layer._zoom) / 40075017,
             map: _layer.map,
-            layer: _layer
+            layer: _layer,
+            scale: 1
         };
 
         // Compatibility with v.1
@@ -114,11 +130,11 @@ L.D3SvgOverlay = (L.version < "1.0" ? L.Class : L.Layer).extend({
         this.draw();
     },
 
-    // Leaf
-    getEvents: function(){return {zoomend: this._zoomChange}},
+    // Leaflet 1.0
+    getEvents: function() { return {zoomend: this._zoomChange}; },
 
     onRemove: function (map) {
-        if (L.version < "1.0"){
+        if (L.version < "1.0") {
             map.off("viewreset", this._zoomChange, this);
             this._rootGroup.remove();
         } else {
