90471a1767bb17fe0ccf30f75be752ce71792dd9
bkr <benediktkromer@gmail.com>
2016-09-13 14:06:03 +0200

    D3 v3 and d3 v4 support

--- L.D3SvgOverlay.js
+++ L.D3SvgOverlay.js
@@ -121,9 +121,15 @@ L.D3SvgOverlay = (L.version < "1.0" ? L.Class : L.Layer).extend({
             var point = _layer.projection.latLngToLayerPoint(new L.LatLng(y, x));
             this.stream.point(point.x, point.y);
         };
-        this.projection.pathFromGeojson =
-            d3.geoPath().projection(d3.geoTransform({point: this.projection._projectPoint}));
+        if(d3.geo){
+           //d3 v3
+            this.projection.pathFromGeojson = d3.geo.path().projection(d3.geo.transform({point: this.projection._projectPoint}));
+        }else{
+           //d3 v4
+           this.projection.pathFromGeojson = d3.geoPath().projection(d3.geoTransform({point: this.projection._projectPoint}));
 
+        }
+        
         // Compatibility with v.1
         this.projection.latLngToLayerFloatPoint = this.projection.latLngToLayerPoint;
         this.projection.getZoom = this.map.getZoom.bind(this.map);
